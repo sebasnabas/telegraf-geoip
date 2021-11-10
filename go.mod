cc0270e384ed99982b1e74d16925e86ba34fa281
sebasnabas <sebasnabas@protonmail.com>
2021-11-10 20:04:20 +0100

    Replace tabs with spaces

--- go.mod
+++ go.mod
@@ -3,6 +3,6 @@ module github.com/a-bali/telegraf-geoip
 go 1.14
 
 require (
-	github.com/IncSW/geoip2 v0.1.0
-	github.com/influxdata/telegraf v1.16.0-rc2
+    github.com/IncSW/geoip2 v0.1.0
+    github.com/influxdata/telegraf v1.16.0-rc2
 )
--- plugins/processors/geoip/geoip.go
+++ plugins/processors/geoip/geoip.go
@@ -1,12 +1,12 @@
 package geoip
 
 import (
-	"fmt"
-	"net"
+    "fmt"
+    "net"
 
-	"github.com/IncSW/geoip2"
-	"github.com/influxdata/telegraf"
-	"github.com/influxdata/telegraf/plugins/processors"
+    "github.com/IncSW/geoip2"
+    "github.com/influxdata/telegraf"
+    "github.com/influxdata/telegraf/plugins/processors"
 )
 
 const sampleConfig = `
@@ -14,82 +14,82 @@ const sampleConfig = `
   db_path = "/var/lib/GeoIP/GeoLite2-City.mmdb"
 
   [[processors.geoip.lookup]
-	# get the ip from the field "source_ip" and put the lookup results in the respective destination fields (if specified)
-	field = "source_ip"
-	dest_country = "source_country"
-	dest_city = "source_city"
-	dest_lat = "source_lat"
-	dest_lon = "source_lon"
+    # get the ip from the field "source_ip" and put the lookup results in the respective destination fields (if specified)
+    field = "source_ip"
+    dest_country = "source_country"
+    dest_city = "source_city"
+    dest_lat = "source_lat"
+    dest_lon = "source_lon"
   `
 
 type lookupEntry struct {
-	Field       string `toml:"field"`
-	DestCountry string `toml:"dest_country"`
-	DestCity    string `toml:"dest_city"`
-	DestLat     string `toml:"dest_lat"`
-	DestLon     string `toml:"dest_lon"`
+    Field       string `toml:"field"`
+    DestCountry string `toml:"dest_country"`
+    DestCity    string `toml:"dest_city"`
+    DestLat     string `toml:"dest_lat"`
+    DestLon     string `toml:"dest_lon"`
 }
 
 type GeoIP struct {
-	DBPath  string          `toml:"db_path"`
-	Lookups []lookupEntry   `toml:"lookup"`
-	Log     telegraf.Logger `toml:"-"`
+    DBPath  string          `toml:"db_path"`
+    Lookups []lookupEntry   `toml:"lookup"`
+    Log     telegraf.Logger `toml:"-"`
 }
 
 var reader *geoip2.CityReader
 
 func (g *GeoIP) SampleConfig() string {
-	return sampleConfig
+    return sampleConfig
 }
 
 func (g *GeoIP) Description() string {
-	return "GeoIP looks up the country code, city name and latitude/longitude for IP addresses in the MaxMind GeoIP database"
+    return "GeoIP looks up the country code, city name and latitude/longitude for IP addresses in the MaxMind GeoIP database"
 }
 
 func (g *GeoIP) Apply(metrics ...telegraf.Metric) []telegraf.Metric {
-	for _, point := range metrics {
-		for _, lookup := range g.Lookups {
-			if lookup.Field != "" {
-				if value, ok := point.GetField(lookup.Field); ok {
-					record, err := reader.Lookup(net.ParseIP(value.(string)))
-					if err != nil {
-						g.Log.Errorf("GeoIP lookup error: %v", err)
-						continue
-					}
-					if len(lookup.DestCountry) > 0 {
-						point.AddField(lookup.DestCountry, record.Country.ISOCode)
-					}
-					if len(lookup.DestCity) > 0 {
-						point.AddField(lookup.DestCity, record.City.Names["en"])
-					}
-					if len(lookup.DestLat) > 0 {
-						point.AddField(lookup.DestLat, record.Location.Latitude)
-					}
-					if len(lookup.DestLon) > 0 {
-						point.AddField(lookup.DestLon, record.Location.Longitude)
-					}
+    for _, point := range metrics {
+        for _, lookup := range g.Lookups {
+            if lookup.Field != "" {
+                if value, ok := point.GetField(lookup.Field); ok {
+                    record, err := reader.Lookup(net.ParseIP(value.(string)))
+                    if err != nil {
+                        g.Log.Errorf("GeoIP lookup error: %v", err)
+                        continue
+                    }
+                    if len(lookup.DestCountry) > 0 {
+                        point.AddField(lookup.DestCountry, record.Country.ISOCode)
+                    }
+                    if len(lookup.DestCity) > 0 {
+                        point.AddField(lookup.DestCity, record.City.Names["en"])
+                    }
+                    if len(lookup.DestLat) > 0 {
+                        point.AddField(lookup.DestLat, record.Location.Latitude)
+                    }
+                    if len(lookup.DestLon) > 0 {
+                        point.AddField(lookup.DestLon, record.Location.Longitude)
+                    }
 
-				}
-			}
-		}
-	}
-	return metrics
+                }
+            }
+        }
+    }
+    return metrics
 }
 
 func (g *GeoIP) Init() error {
-	r, err := geoip2.NewCityReaderFromFile(g.DBPath)
-	if err != nil {
-		return fmt.Errorf("Error opening GeoIP database: %v", err)
-	} else {
-		reader = r
-	}
-	return nil
+    r, err := geoip2.NewCityReaderFromFile(g.DBPath)
+    if err != nil {
+        return fmt.Errorf("Error opening GeoIP database: %v", err)
+    } else {
+        reader = r
+    }
+    return nil
 }
 
 func init() {
-	processors.Add("geoip", func() telegraf.Processor {
-		return &GeoIP{
-			DBPath: "/var/lib/GeoIP/GeoLite2-Country.mmdb",
-		}
-	})
+    processors.Add("geoip", func() telegraf.Processor {
+        return &GeoIP{
+            DBPath: "/var/lib/GeoIP/GeoLite2-Country.mmdb",
+        }
+    })
 }
